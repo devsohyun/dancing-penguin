46d24f293871d7ba09b842dbef09a6a65577c2e7
sohjune <64575677+sohjune@users.noreply.github.com>
2020-06-05 18:47:00 +0900

    put buffer and changed colors

---
+++ DancingPenguin/Assets/Scripts/Buffer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buffer : MonoBehaviour
+{
+    public int band;
+    public float startScale, scaleMultiplier;
+    public bool useBuffer;
+    public GameObject penguin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (useBuffer)  {
+                penguin.transform.localScale = new Vector3(penguin.transform.localScale.x, (MicrophoneInput.bandBuffer [band] * scaleMultiplier) + startScale, penguin.transform.localScale.z);   
+            }
+
+            if (!useBuffer)  {
+                penguin.transform.localScale = new Vector3(penguin.transform.localScale.x, (MicrophoneInput.freqBand [band] * scaleMultiplier) + startScale, penguin.transform.localScale.z);   
+                //penguin.transform.localScale = new Vector3(2.0f,  bandBuffer * Mathf.Abs(Mathf.Log(spectrum[spectrumIndex]))*scaleFactor, 1.0f);
+            }
+        
+    }
+}
--- DancingPenguin/Assets/Scripts/MicrophoneInput.cs
+++ DancingPenguin/Assets/Scripts/MicrophoneInput.cs
@@ -15,12 +15,25 @@ public class MicrophoneInput : MonoBehaviour
     public string selectedDevice;
     public int audioSampleRate = 44100;
     public string microphone;
+    public AudioMixerGroup mixerGroupMicrophone, mixerGroupMaster;   
+
+    //Buffer
+    
+    public static float[] freqBand = new float[8];
+    public static float[] bandBuffer = new float[8];
+    private float[] bufferDecrease = new float[8]; 
+    public bool useBuffer;
+    public int band;
+
+
+    
 
     public GameObject box;
     public GameObject penguin;
 
     [Header("Settings")]
-    public float scaleFactor = 2.0f;
+    public float scaleFactor = -2.0f;
+    
     public int spectrumIndex = 14;
 
     
@@ -38,7 +51,7 @@ public class MicrophoneInput : MonoBehaviour
                 microphone = device;
             }
         }
-        //UpdateMicrophone();
+        
 
         //find devices microphone name 
         foreach (var device in Microphone.devices)
@@ -52,7 +65,7 @@ public class MicrophoneInput : MonoBehaviour
             if (Microphone.devices.Length > 0 )
             {
                 Debug.Log ("microphoneInput");
-            
+                audioSource.outputAudioMixerGroup = mixerGroupMicrophone; //when sounds makes no eco from me
                 //get microphone at auidoClip
                 audioSource.clip = Microphone.Start(Microphone.devices[0].ToString(), true, 10, audioSampleRate);
                 //GetComponent<Renderer>().material.color = Color.red;
@@ -68,6 +81,7 @@ public class MicrophoneInput : MonoBehaviour
         if (!useMicrophone)
         {
             audioSource.clip = audioClip;
+            audioSource.outputAudioMixerGroup = mixerGroupMaster; //when sounds makes no eco from me
         }
     
      audioSource.Play();
@@ -77,21 +91,54 @@ public class MicrophoneInput : MonoBehaviour
     void Update()
     {
         if(audioSource != null){
-            float[] spectrum = new float[64];
+            
+            float[] spectrum = new float[512];
 
             audioSource.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
 
             for (int i = 1; i < spectrum.Length - 1; i++)
             {
                 // Debug.DrawLine(new Vector3(i - 1, spectrum[i] + 10, 0), new Vector3(i, spectrum[i + 1] + 10, 0), Color.red);
-               // Debug.DrawLine(new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2), new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2), Color.cyan);
+                // Debug.DrawLine(new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2), new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2), Color.cyan);
                 // Debug.DrawLine(new Vector3(Mathf.Log(i - 1), spectrum[i - 1] - 10, 1), new Vector3(Mathf.Log(i), spectrum[i] - 10, 1), Color.green);
                 // Debug.DrawLine(new Vector3(Mathf.Log(i - 1), Mathf.Log(spectrum[i - 1]), 3), new Vector3(Mathf.Log(i), Mathf.Log(spectrum[i]), 3), Color.blue);
-            }
             
-            penguin.transform.localScale = new Vector3(4.0f,  Mathf.Abs(Mathf.Log(spectrum[spectrumIndex]))*scaleFactor, 4);
+            }
+
+            penguin.transform.localScale = new Vector3(2.0f, Mathf.Abs(Mathf.Log(spectrum[spectrumIndex]))*scaleFactor, 2.0f);
+            
+            /*
+            if (useBuffer)  {
+                penguin.transform.localScale = new Vector3(2.0f,  bandBuffer [band] * Mathf.Abs(Mathf.Log(spectrum[spectrumIndex]))*scaleFactor, 2.0f);   
+            }
+
+            if (!useBuffer)  {
+                penguin.transform.localScale = new Vector3(2.0f,  freqBand [band] * Mathf.Abs(Mathf.Log(spectrum[spectrumIndex]))*scaleFactor, 2.0f);   
+                //penguin.transform.localScale = new Vector3(2.0f,  bandBuffer * Mathf.Abs(Mathf.Log(spectrum[spectrumIndex]))*scaleFactor, 1.0f);
+            }
+            */
+
         }
         
+        BandBuffer();
+
+     
+        
+    }
+
+    void BandBuffer()
+    {
+        for (int g = 0; g < 8; ++g) {
+            if (freqBand [g] > bandBuffer [g]){
+                bandBuffer [g] = freqBand [g] ;
+                bufferDecrease [g] = 0.005f;
+            }
+            if (freqBand [g] < bandBuffer [g]){
+                bandBuffer [g] -= bufferDecrease [g];
+                bufferDecrease [g] *= 1.2f;
+            }
+
+        }
     }
 
     void ChangeColors()
